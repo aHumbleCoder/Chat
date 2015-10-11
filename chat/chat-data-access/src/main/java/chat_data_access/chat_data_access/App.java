6fbffbc9e22a26b71a8334fb6c1e1839b392b37a
LT <ahumblecoder@gmail.com>
2015-10-11 16:44:46 +0800

    recreate project skeleton(multi-module project

---
+++ chat/chat-data-access/src/main/java/chat_data_access/chat_data_access/App.java
@@ -0,0 +1,13 @@
+package chat_data_access.chat_data_access;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
